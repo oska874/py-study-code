a9509f5ee12ad08e335362c7feaac2b2594b4a07
ezio <ezio_zhang@outlook.com>
2015-09-09 21:32:17 +0800

    socket test.

--- stage_4/socket0.py
+++ stage_4/socket0.py
@@ -14,3 +14,13 @@ print(socket.getprotobyname("udplite"))
 
 s1 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 print(s1)
+
+print("host : "+socket.gethostbyname("ezio-HP-ENVY-15-Notebook-PC"))
+
+print("ip n : "+socket.inet_ntoa(socket.inet_aton("127.0.0.1")))
+print(	s1.fileno())
+print("socket name:" + s1.getsockname()[0])
+
+#print(s1.getsockopt())
+
+print("timeout: "+s1.gettimeout())
