74a66c83a3e13db86be30e975ca5aac753c06fc0
ezio <ezio_zhang@outlook.com>
2015-09-14 21:23:40 +0800

    calcuate .c src line,comment line,empty line.

--- utils/line_count/get_src_file.py
+++ utils/line_count/get_src_file.py
@@ -10,23 +10,24 @@ import count_lines
 if __name__ == "__main__":
     argn=len(sys.argv)
     if argn < 2:
-            print("usage:get_src_file.py [t]<t> <dir>")
-            exit
+        print("usage:get_src_file.py [t]<t> <dir>")
+        exit
 
     # get filename
     # check recurise walk dir
     # select specific type files
 
 
+
     if sys.argv[1] != 't':
-            print("parameters error")
-            exit	
+        print("parameters error")
+        exit	
     else:		
-            cur_dir = sys.argv[3]
-            stype = sys.argv[2]
+        cur_dir = sys.argv[3]
+        stype = sys.argv[2]
 
     if os.path.isdir(sys.argv[3]) == True:
-            print("dir : "+str(True))
+        print("dir : "+str(True))
 
     fnames = os.walk(cur_dir)
 
@@ -35,17 +36,23 @@ if __name__ == "__main__":
 
     total_line = 0
 
+    l1 = 0
+    l2 = 0
+    l3 = 0
     for fn in fnames:
-            for x in fn[2]:
-                if sys.platform == "linux2":
-			if cur_dir[-1] != '/':
-				res = stRe.match(cur_dir + "/" + x)
-			else:
-				res = stRe.match(cur_dir + x)
+        for x in fn[2]:
+            if sys.platform == "linux2":
+                if cur_dir[-1] != '/':
+                    res = stRe.match(cur_dir + "/" + x)
+                else:
+                    res = stRe.match(cur_dir + x)
 				
                 if res:
                     print(res.group())
-                    total_line += count_lines.count_line1(res.group(),stype)
+                    aa = count_lines.count_line1(res.group(),stype)
+                    l1 += aa[0]
+                    l2 += aa[1]
+                    l3 += aa[2]
+    print("source %d\ncomment %d\nempty %d" % (l1,l2,l3))
 
-    print("total line : " + str(total_line))
 
