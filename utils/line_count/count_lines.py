cdf7f0f9529a3a86cad68386a9410e5a149c7891
ezio <ezio_zhang@outlook.com>
2015-09-14 21:23:40 +0800

    calcuate .c src line,comment line,empty line.

--- utils/line_count/count_lines.py
+++ utils/line_count/count_lines.py
@@ -7,44 +7,46 @@
 import sys,os
 
 def count_line1(fna,stype):
-	l1 = 0
-	fd1 = open(fna,'r')
-	divide_lines(fd1,stype)
-        fd1.close()
-	return l1
+    fd1 = open(fna,'r')
+    ret = divide_lines(fd1,stype)
+    fd1.close()
+    return ret 
 
 
 def divide_lines(fd,ft):
-	l1 = 0 # source
-	l2 = 0 # comment
-	l3 = 0 # empty
-	if ft == 'c' or ft == 'cpp':
-		multi_comment = 0
-		print("this is c/c++ files")
-		for line in fd:
-			if len(l2) > 1:
-				if multi_comment == 1:
-					if l2[-1] == '/' and l2[-2] == '*':
-						l2 += 1
-						multi_comment = 0
-				elif multi_comment == 0:
-					if l2[0] == '/' and l2[1] == '/':
-						l2 += 1
-					elif l2[0] == '/' and l2[1] == '*':
-						l2 += 1
-						multi_comment = 1
-					else:
-						l1 += 1
-			else :
-				l3 += 1
-		print("source %d, comment %d, empty %d" % l1,l2,l3)
+    l1 = 0 # source
+    l2 = 0 # comment
+    l3 = 0 # empty
 
-	elif ft == 'py':
-		print("this is python files")
-	else:
-		print("not supported files "+ft)
-
-
+    if ft == 'c' or ft == 'cpp':
+        multi_comment = 0
+        print("this is c/c++ files")
+        for line in fd:
+            if len(line) > 1:
+                if multi_comment == 0:
+                    # comment line start from "/*" or single line comment "//"
+                    if line[0] == '/' and line[1] == '*':
+                        l2 += 1
+                        multi_comment = 0
+                    elif line[0] == '/' and line[1] == '/':
+                        l2 += 1
+                    else :
+                        # source line
+                        l1 += 1
+                elif multi_comment == 1:
+                    # comment line till meet "*/"
+                    l2 += 1
+                    if line[-2] == '*' and line[-1] == '/':
+                        multi_comment = 0
+            else :
+                # empty line
+                l3 += 1
+        print("source %d, comment %d, empty %d" % (l1,l2,l3))
+    elif ft == 'py':
+        print("this is python files")
+    else:
+        print("not supported files "+ft)
+    return (l1,l2,l3)
 
 if __name__ == "__main__":
     argn = len(sys.argv)
@@ -61,12 +63,12 @@ if __name__ == "__main__":
     for line in fd:
         if len(line) > 1:
             ln+=1
-    fd.close()
-    print(filename+"has %d lines" % ln);
-	if len(l2) > 1:
-	if l2[0] == '/' and l2[1] == '/':
-	l1 += 0
-	elif l2[0] == '/' and l2[1] == '*':
-	l1 += 0
-	else:
-	l1 += 1
+            fd.close()
+            print(filename+"has %d lines" % ln);
+            if len(l2) > 1:
+                if l2[0] == '/' and l2[1] == '/':
+                        l1 += 0
+                elif l2[0] == '/' and l2[1] == '*':
+                        l1 += 0
+                else:
+                        l1 += 1
