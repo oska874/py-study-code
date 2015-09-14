93e4f4e5272b085f8f9a809f555e825a0fd53f2f
ezio <ezio_zhang@outlook.com>
2015-09-14 10:54:48 +0800

    line counter now: just count non-empty lines

--- utils/count_lines.py
+++ utils/count_lines.py
@@ -6,26 +6,29 @@
 
 import sys,os
 
-
-# argn = len(sys.argv)
-# if argn < 2:
-# 	print("usage:python count_lines.py <filename>")
-# 	exit
-
-# #get filename
-# filename = sys.argv[1]
-# print("source file: "+filename)
-
-# ln = 0
-# fd = open(filename,'r')
-# for line in fd:
-# 	if len(line) > 1:
-# 		ln+=1
-
 def count_line1(fna):
 	l1 = 0
 	fd1 = open(fna,'r')
 	for l2 in fd1:
 		if len(l2) > 1:
 			l1 += 1
+        fd1.close()
 	return l1
+
+if __name__ == "__main__":
+    argn = len(sys.argv)
+    if argn < 2:
+        print("usage:python count_lines.py <filename>")
+        exit
+
+    #get filename
+    filename = sys.argv[1]
+    print("source file: "+filename)
+
+    ln = 0
+    fd = open(filename,'r')
+    for line in fd:
+        if len(line) > 1:
+            ln+=1
+    fd.close()
+    print(filename+"has %d lines" % ln);
