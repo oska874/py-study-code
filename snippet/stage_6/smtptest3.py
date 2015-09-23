cd6d71e7942a85dbdbd18246011e424887b74068
ezio <ezio_zhang@outlook.com>
2015-09-23 23:21:34 +0800

    smtp test:send email, attatchment

--- snippet/stage_6/smtptest3.py
+++ snippet/stage_6/smtptest3.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
@@ -9,28 +8,22 @@ import smtplib
 msg = MIMEMultipart()
 
 #构造附件1
-att1 = MIMEText(open('d:\\123.rar', 'rb').read(), 'base64', 'gb2312')
+att1 = MIMEText(open('./README.md', 'rb').read(), 'base64', 'gb2312')
 att1["Content-Type"] = 'application/octet-stream'
-att1["Content-Disposition"] = 'attachment; filename="123.doc"'#这里的filename可以任意写，写什么名字，邮件中显示什么名字
+att1["Content-Disposition"] = 'attachment; filename="read.txt"'#这里的filename可以任意写，写什么名字，邮件中显示什么名字
 msg.attach(att1)
 
-#构造附件2
-att2 = MIMEText(open('d:\\123.txt', 'rb').read(), 'base64', 'gb2312')
-att2["Content-Type"] = 'application/octet-stream'
-att2["Content-Disposition"] = 'attachment; filename="123.txt"'
-msg.attach(att2)
-
 #加邮件头
-msg['to'] = 'YYY@YYY.com'
-msg['from'] = 'XXX@XXX.com'
+msg['to'] = '***@126.com'
+msg['from'] = '***@126.com'
 msg['subject'] = 'hello world'
 #发送邮件
 try:
     server = smtplib.SMTP()
-    server.connect('smtp.XXX.com')
-    server.login('XXX','XXXXX')#XXX为用户名，XXXXX为密码
+    server.connect('smtp.126.com')
+    server.login('***','***')#XXX为用户名，XXXXX为密码
     server.sendmail(msg['from'], msg['to'],msg.as_string())
     server.quit()
-    print '发送成功'
+    print('发送成功')
 except Exception, e:  
-    print str(e) 
+    print(str(e)) 
