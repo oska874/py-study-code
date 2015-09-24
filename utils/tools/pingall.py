8567d9232ab194a77c6f25d1a29de931c50b90b4
ezio <ezio_zhang@outlook.com>
2015-09-24 18:02:49 +0800

    update pingall.py

--- utils/tools/pingall.py
+++ utils/tools/pingall.py
@@ -1,6 +1,24 @@
-import os
+# -*- coding: utf-8 -*- 
 
+import os,sys
+
+# used in windows
 for i in range(1,254):
 	print("ping :"+str(i))
-	os.system("ping -n 1 192.168.0."+str(i))
+	os.system("ping -n 1 192.168.0."+str(i) + "> ./aa")
 	print("end")
+	if i % 2 == 0:
+		os.system("arp -a >./ffff")
+		fd = open("./ffff",'r')
+		for x in fd:
+			if x.find("00-04-9f-03-ae-ef") :
+				print(x)
+				fd.close()
+				os.system('rm ffff;rm aa')
+				exit()
+		fd.close()
+		os.system('rm ffff')
+	os.system('rm aa')
+
+
+
