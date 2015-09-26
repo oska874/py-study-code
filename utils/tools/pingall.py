4901f2fc96cb7bbc95106ae54c6dcfe3aae4d905
ezio <ezio_zhang@outlook.com>
2015-09-26 21:25:53 +0800

    bug fix

--- utils/tools/pingall.py
+++ utils/tools/pingall.py
@@ -4,18 +4,20 @@ import os,sys
 
 # used in windows
 for i in range(1,254):
-	print("ping :"+str(i))
-	os.system("ping -n 1 192.168.0."+str(i) + "> ./aa")
+    print("ping :"+str(i))
+    if sys.platform == 'win':
+        os.system("ping -n 1 192.168.0."+str(i) + "> ./aa")
 	print("end")
-	if i % 2 == 0:
+	if i % 13 == 0:
+            if sys.platform == 'win':
 		os.system("arp -a >./ffff")
 		fd = open("./ffff",'r')
 		for x in fd:
-			if x.find("00-04-9f-03-ae-ef") :
-				print(x)
-				fd.close()
-				os.system('rm ffff;rm aa')
-				exit()
+                    if x.find("00-04-9f-03-ae-ef") >= 0:
+                        print(x)
+                        fd.close()
+                        os.system('rm ffff;rm aa')
+                        exit()
 		fd.close()
 		os.system('rm ffff')
 	os.system('rm aa')
