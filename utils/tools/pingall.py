1d35bcc51689b3ec84e4d33fc183343dc4713ab9
ezio <ezio_zhang@outlook.com>
2015-09-28 10:09:13 +0800

    conflict solved

--- utils/tools/pingall.py
+++ utils/tools/pingall.py
@@ -4,21 +4,23 @@ import os,sys
 
 # used in windows
 for i in range(1,254):
-	print("ping :"+str(i))
-	os.system("ping -n 1 192.168.0."+str(i) + "> ./aa")
-	print("end")
-	if i % 2 == 0:
-		os.system("arp -a >./ffff")
-		fd = open("./ffff",'r')
-		for x in fd:
-			if x.find("00-04-9f-03-ae-ef") >= 0 :
-				print(x)
-				fd.close()
-				os.system('rm ffff; rm aa')
-				exit()
-		fd.close()
-		os.system('rm ffff')
-	os.system('rm aa')
+    print("ping :"+str(i))
+    if sys.platform == 'win':
+        os.system("ping -n 1 192.168.0."+str(i) + "> ./aa")
+    else if sys.platform == 'linux2':
+        os.system("ping -c 1 192.168.0." + str(i) + ">./aa")
+    print("end")
 
-
-
+    if i % 13 == 0:
+        if sys.platform == 'win':
+            os.system("arp -a >./ffff")
+            fd = open("./ffff",'r')
+            for x in fd:
+                if x.find("00-04-9f-03-ae-ef") >= 0:
+                    print(x)
+                    fd.close()
+                    os.system('rm ffff;rm aa')
+                    exit()
+            fd.close()
+            os.system('rm ffff')
+    os.system('rm aa')
