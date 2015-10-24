3872040287476bf433493cd6f8bd15c484294c4d
Ezio <ezio_zhang@outlook.com>
2015-10-25 00:40:32 +0800

    update

---
+++ snippet/gui_test/tki_mainloop.py
@@ -0,0 +1,11 @@
+from Tkinter import *
+
+root = Tk()
+top = Toplevel()
+top2 = Toplevel()
+
+root.title("O0")
+top.title("O1")
+top2.title("O2")
+top.mainloop()
+top2.mainloop()
---
+++ snippet/gui_test/tki_spinbox.py
@@ -0,0 +1,8 @@
+from Tkinter import *
+
+master = Tk()
+
+w = Spinbox(master, from_=0, to=10)
+w.pack()
+
+mainloop()
