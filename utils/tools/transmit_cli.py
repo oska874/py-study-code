a1bf3eacd63dac5e79b041149133acf21e4b31fc
Ezio <ezio_zhang@outlook.com>
2015-10-25 12:19:23 +0800

    get widget's value and passed to cli function

--- utils/tools/transmit_cli.py
+++ utils/tools/transmit_cli.py
@@ -1,15 +1,53 @@
+import tftpy as tftp
+import pyftpdlib as ftp
+import http as http
+import re
 
 
-def my_tftpd():
-	print("start tftp")
+def valid_ip(ip):
+	ipRe = re.compile("([0-9]{1,3}\.){3}[0-9]{1,3}$") #basic rule,and need improvement
+	if ipRe.match(ip) != None:
+		#print("get ip: "+ip)
+		return 0
+	else:
+		return -1
 
-def my_ftpd():
-	print("start ftp")
 
-def my_httpd():
-	print("start http")
+def valid_port(port):
+	portRe = re.compile("[0-9]+$")
+	if portRe.match(port) != None:
+		#print("get port : "+port)
+		return 0
+	else:
+		return -1
+def my_tftpd(ip=0,port=0):
+	#print("start tftp"+ip+":"+port)
+	if valid_port(port) == 0 and valid_ip(ip) == 0:
+		print("start tftpd : "+ip+":"+port)
 
-def my_socketd():
-	print("start socket")
+		return 0
+	else:
+		return -1
 
+def my_ftpd(ip=0,port=0):
+	#print("start ftp"+ip+":"+port)
+	if valid_port(port) == 0 and valid_ip(ip) == 0:
+		print("start ftpd : "+ip+":"+port)
+		return 0
+	else:
+		return -1
+def my_httpd(ip=0,port=0):
+	#print("start http"+ip+":"+port)
+	if valid_port(port) == 0 and valid_ip(ip) == 0:
+		print("start httpd : "+ip+":"+port)
+		return 0
+	else:
+		return -1
+def my_socketd(ip=0,port=0):
+	#print("start socket"+ip+":"+port)
+	if valid_port(port) == 0 and valid_ip(ip) == 0:
+		print("start socketd : "+ip+":"+port)
+		return 0
+	else:
+		return -1
 
