b44659560df70b87f8ceb29ebf788e22131b042e
ezio <ezio_zhang@outlook.com>
2015-11-04 11:00:14 +0800

    pyside mainwindow test add some components

--- snippet/gui_test/pyside/pyside_mainwindow0.py
+++ snippet/gui_test/pyside/pyside_mainwindow0.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # Import required modules
 import sys, time
-from PySide.QtGui import QApplication, QMainWindow,QStatusBar,QLabel,QProgressBar
+from PySide.QtGui import QApplication, QMainWindow,QStatusBar,QLabel,QProgressBar,QTextEdit,QMenuBar,QKeySequence,QAction,QIcon,QMessageBox,QToolBar
 
 class MainWindow(QMainWindow):
 	""" Our Main Window Class
@@ -34,7 +34,79 @@ class MainWindow(QMainWindow):
 			self.progressBar.setValue(self.progressBar.value() + 10)
 			time.sleep(1)
 		self.statusLabel.setText('Ready')
+		self.progressBar.hide()
 
+	def SetupComponents(self):
+		""" Setting the central widget
+		"""
+		#add textedit in central
+		self.textEdit = QTextEdit()
+		self.setCentralWidget(self.textEdit)
+		self.CreateActions()
+		#add menubar 
+		self.CreateMenus()
+		#add action for each menu
+		self.fileMenu.addAction(self.newAction)
+		self.fileMenu.addSeparator()
+		self.fileMenu.addAction(self.exitAction)
+		self.editMenu.addAction(self.copyAction)
+		self.fileMenu.addSeparator()
+		self.editMenu.addAction(self.pasteAction)
+		self.helpMenu.addAction(self.aboutAction)
+		#add toolbar
+		self.CreateToolBar()
+		self.mainToolBar.addAction(self.newAction)
+		self.mainToolBar.addSeparator()
+		self.mainToolBar.addAction(self.copyAction)
+		self.mainToolBar.addAction(self.pasteAction)
+
+	# Slots called when the menu actions are triggered
+	def newFile(self):
+		self.textEdit.setText('')
+
+	def exitFile(self):
+		self.close()
+
+	def aboutHelp(self):
+		QMessageBox.about(self, "About Simple Text Editor",
+		"This example demonstrates the use of Menu Bar")
+
+	def CreateActions(self):
+		""" Function to create actions for menus
+		"""
+		#function divided into two parts: built-in and new defined
+		self.newAction = QAction( QIcon('new.png'), '&New', 
+			self, shortcut=QKeySequence.New,
+			statusTip="Create a New File",triggered=self.newFile)
+
+		self.exitAction = QAction( QIcon('exit.png'), 'E&xit',
+			self, shortcut="Ctrl+Q", statusTip="Exit the Application",
+			triggered=self.exitFile)
+
+		self.copyAction = QAction( QIcon('copy.png'), 'C&opy',
+			self, shortcut="Ctrl+C", statusTip="Copy",
+			triggered=self.textEdit.copy)
+
+		self.pasteAction = QAction( QIcon('paste.png'), '&Paste',
+			self, shortcut="Ctrl+V",statusTip="Paste",
+			triggered=self.textEdit.paste)
+
+		self.aboutAction = QAction( QIcon('about.png'), 'A&bout',
+			self, statusTip="Displays info about text editor",
+			triggered=self.aboutHelp)
+
+	# Actual menu bar item creation
+	def CreateMenus(self):
+		""" Function to create actual menu bar
+		"""
+		self.fileMenu = self.menuBar().addMenu("&File")
+		self.editMenu = self.menuBar().addMenu("&Edit")
+		self.helpMenu = self.menuBar().addMenu("&Help")
+
+	def CreateToolBar(self):
+		""" Function to create tool bar
+		"""
+		self.mainToolBar = self.addToolBar('Main')
 
 
 ################
@@ -44,6 +116,7 @@ if __name__ == '__main__':
 		myApp = QApplication(sys.argv)
 		mainWindow = MainWindow()
 		mainWindow.CreateStatusBar()
+		mainWindow.SetupComponents()
 		mainWindow.show()
 		mainWindow.ShowProgress()
 		myApp.exec_()
