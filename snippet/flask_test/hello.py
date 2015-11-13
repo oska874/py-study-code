7951bcdaad26f5b516b092780dfe5048ddc453d8
ezio <ezio_zhang@outlook.com>
2015-11-13 10:59:33 +0800

    update flask test

--- snippet/flask_test/hello.py
+++ snippet/flask_test/hello.py
@@ -1,9 +1,30 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+#@app.route('/hello')
+#def hello():
+#    return 'Hello World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0',debug=True)
