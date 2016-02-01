ea3b583763560a558bcd6a2d93d241e9e993860c
ezio <ezio_zhang@outlook.com>
2016-02-01 22:47:47 +0800

    update

--- snippet/flask_test/hello.py
+++ snippet/flask_test/hello.py
@@ -1,12 +1,21 @@
 #-*- coding:utf8 -*-
-from flask import Flask,request
-app = Flask(__name__)
-
+from flask import make_response,abort,redirect
+from werkzeug import secure_filename
+from flask import Flask,request,url_for
 from flask import render_template
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+app = Flask(__name__)
+
+@app.route('/notfound')
+def errp():
+    abort(401)
+
+@app.route('/hello/') #绑定函数到对应的url
+@app.route('/hello/<name>') #`<name>` 可以作为参数传给hello，支持三种类型：int、float、path
 def hello(name=None):
+    if name == 'error':
+        print(url_for('hello'))
+        return redirect(url_for('hello'))
     return render_template('hello.html', name=name)
 
 @app.route('/')
@@ -26,10 +35,6 @@ def login():
     # 如果请求访求是 GET 或验证未通过就会执行下面的代码
 	return render_template('login.html', error=error)
 
-#@app.route('/hello')
-#def hello():
-#    return 'Hello World'
-
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
@@ -39,6 +44,8 @@ def show_user_profile(username):
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
