297fc3d2bef46ab8869eaec5d8dfbf7afbdec84b
ezio <ezio_zhang@outlook.com>
2016-05-25 23:28:58 +0800

    add net test

---
+++ snippet/storage/cpr0.py
@@ -0,0 +1,12 @@
+import copy_reg, copy, pickle
+class C(object):
+    def __init__(self, a):
+        self.a = a
+def pickle_c(c):
+    print("pickling a C instance...")
+    return C, (c.a,)
+
+copy_reg.pickle(C, pickle_c)
+c = C(1)
+d = copy.copy(c)
+p = pickle.dumps(c)
