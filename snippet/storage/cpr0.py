eba52c83e3af65015a6ce26912d744396b7fbb1a
ezio <ezio_zhang@outlook.com>
2017-07-23 15:41:34 +0800

    update

--- snippet/storage/cpr0.py
+++ snippet/storage/cpr0.py
@@ -1,3 +1,5 @@
+#used in python 2.7
+#in python 3 ,rename copy_reg to copyreg
 import copy_reg, copy, pickle
 class C(object):
     def __init__(self, a):
@@ -7,6 +9,7 @@ def pickle_c(c):
     return C, (c.a,)
 
 copy_reg.pickle(C, pickle_c)
+
 c = C(1)
 d = copy.copy(c)
 p = pickle.dumps(c)
